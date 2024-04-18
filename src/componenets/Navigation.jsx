090bfd97e0ab1f8134b09e388ef68a2a5bf01f46
dylanR96 <dylan.ravenson@gmail.com>
2024-04-18 22:34:45 +0200

    Addded more style and structure, testing useState for navigation menu

--- src/componenets/Navigation.jsx
+++ src/componenets/Navigation.jsx
@@ -8,21 +8,48 @@ function Navigation() {
   const lowerMenu = () => {
     setTest(true);
   };
+
+  const removeMenu = () => {
+    setTest(false);
+  };
+
   return (
     <>
       <aside className="main__navigation">
         <div className="main__navigation-container">
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/myPages">My pages</Link>
-            <Link to="/expenses" onClick={lowerMenu}>
+          <nav className="main__upper-navigation">
+            <Link className="main_nav-links" to="/" onClick={removeMenu}>
+              Home
+            </Link>
+            <Link className="main_nav-links" to="/myPages" onClick={removeMenu}>
+              My pages
+            </Link>
+            <Link className="main_nav-links" to="/expenses" onClick={lowerMenu}>
               Expenses
             </Link>
-            <Link to="/stocks">Stocks</Link>
-            <Link to="/savingPlan">Saving plans</Link>
+            <Link className="main_nav-links" to="/stocks" onClick={removeMenu}>
+              Stocks
+            </Link>
+            <Link
+              className="main_nav-links"
+              to="/savingPlan"
+              onClick={removeMenu}
+            >
+              Saving plans
+            </Link>
           </nav>
           <div className="main__nav-divider"></div>
-          <div>{test ? <div>hello</div> : <div></div>}</div>
+          <div>
+            {test ? (
+              <div className="main__lower-navigation">
+                <button>My expenses</button>
+                <button>Total expenses</button>
+                <button>Total savings</button>
+              </div>
+            ) : (
+              <div></div>
+            )}
+          </div>
         </div>
       </aside>
     </>
