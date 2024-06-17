c0820483c4346b67a8550358a4005897ebafe6c9
dylanR96 <dylan.ravenson@gmail.com>
2024-06-17 14:34:06 +0200

    Changed routing/navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <>
       <div className="main">
-        <Router>
-          <Navigation />
-        </Router>
+        <Navigation />
+        <Outlet />
       </div>
     </>
   );
