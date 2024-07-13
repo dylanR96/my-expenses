188bccb92ee37187bdef59c536bd343833380a59
dylanR96 <dylan.ravenson@gmail.com>
2024-07-13 22:19:37 +0200

    Added new page for expense table, added table

---
+++ src/components/Expenses/ExpenseTable.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ExpenseTable = () => {
+  return (
+    <>
+      <div>
+        <h3>New Expense</h3>
+        <button onClick={expenseform}>X</button>
+      </div>
+    </>
+  );
+};
+
+export default ExpenseTable;
